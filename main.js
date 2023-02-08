cb6eaddd5833a2dd5eac2e4b7269f55cc57a8f1a
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-02-07 19:05:10 -0500

    Track titles play music demo when clicked

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-//Variable--Contained elements from HTML---Contained URL parts
+// Variable--Contained elements from HTML---Contained URL parts
 const form = document.querySelector("#search-form");
 const baseUrl = "https://proxy-itunes-api.glitch.me/search?term=";
 const endUrl = "&media=music&entity=song&attribute=artistTerm";
 const container = document.querySelector("#musicContainer");
+const musicPlayer = document.querySelector("#audio-player");
 
-
-//Event---Listen for search being submitted
+// Event---Listen for search being submitted
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     let term = document.querySelector("#search-text").value;
@@ -13,7 +13,7 @@ form.addEventListener("submit", function (event) {
     search(term);
 });
 
-//Function---Gather results from API---Run buildResultHtml function
+// Function---Gather results from API---Run buildResultHtml function
 function search(searchTerm) {
     let searchUrl = `${baseUrl}${searchTerm}${endUrl}`;
     console.log(searchUrl);
@@ -29,40 +29,46 @@ function search(searchTerm) {
         });
 }
 
-//Function---What to display from API results
+// Function---What to display from API results
 function buildResultsHtml(resultArray) {
     for (let spot of resultArray) {
-        ///div for all song content
+
+        // div for all song content
         let songDiv = document.createElement("div")
         songDiv.classList.add("songDiv");
 
-        //Artist Name---
-        let artistName = document.createElement("p")
-        artistName.classList.add("artist-name")
-        artistName.innerText = spot.artistName;
-
-        //Track Name---
+        // Track Name---
         let trackTitle = document.createElement("p");
         trackTitle.classList.add("track-name");
         trackTitle.innerText = `Track: ${spot.trackName}`;
 
-        //Album Title---
+        // Click-Event for each Track Title
+        trackTitle.addEventListener('click', function (event) {
+            let demo = spot.previewUrl;
+            musicPlayer.src = demo;
+        })
+
+        // Artist Name---
+        let artistName = document.createElement("p")
+        artistName.classList.add("artist-name")
+        artistName.innerText = spot.artistName;
+
+        // Album Title---
         let albumTitle = document.createElement("p")
         albumTitle.classList.add("album-title");
         albumTitle.innerText = `Album: ${spot.collectionName}`;
 
-        //Album Art---
+        // Album Art---
         let coverThumbnail = document.createElement("img")
         coverThumbnail.classList.add("cover-art");
         coverThumbnail.src = spot.artworkUrl100;
 
-        //Append to container---
-        songDiv.appendChild(artistName);
+        // Append to container---
         songDiv.appendChild(trackTitle);
+        songDiv.appendChild(artistName);
         songDiv.appendChild(albumTitle);
         songDiv.appendChild(coverThumbnail);
         container.appendChild(songDiv);
 
     }
 }
-
